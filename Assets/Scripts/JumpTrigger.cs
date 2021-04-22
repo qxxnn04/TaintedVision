7c6e8a29c6d40f51022491da24d2460135843cf5
qxxnn04 <500577@chrome.escg.ac.uk>
2021-04-22 16:11:49 +0100

    Jumpscares

---
+++ Assets/Scripts/JumpTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpTrigger : MonoBehaviour
+{
+    public AudioSource Scream;
+    public GameObject Player;
+    public GameObject JumpCam;
+    public GameObject FlashIng;
+
+    void OnTriggerEnter ()
+    {
+        Scream.Play();
+        JumpCam.SetActive(true);
+        Player.SetActive(false);
+        FlashIng.SetActive(true);
+        StartCoroutine(EndJump ());
+    }
+
+    IEnumerator EndJump ()
+    {
+        yield return new WaitForSeconds(2.03f);
+        Player.SetActive(true);
+        JumpCam.SetActive(false);
+        FlashIng.SetActive(false);
+
+    }
+
+}
