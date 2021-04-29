60fe39cd995c348657b661d0652fbacd6265e8fb
qxxnn04 <500577@chrome.escg.ac.uk>
2021-04-29 12:01:44 +0100

    Game end function

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,20 +11,11 @@ public class Player : MonoBehaviour
     public GameObject JumpCam;
     public GameObject Crawler;
 
+    public int score;
+
     [Header("Visuals")]
     public Camera playerCamera;
-    public GameObject bulletPrefab;
 
-    [Header("Gameplay")]
-    public int initialHealth = 100;
-    public int initialScore = 24;
-
-
-
-
-    private int health;
-    public int Health { get { return health; } }
-    private int score;
     public int Score { get { return score; } }
 
     public Enemy enemy;
@@ -50,9 +41,11 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+      
         score = 0;
-        health = 100;
     }
+
+
 
 
     void OnCollisionEnter(Collision collision)
